1ff4f50953ba6594af756031fdae489348d661c0
timur.borisevich <timurborisevich@mail.ru>
2020-12-05 18:41:49 +0300

    new way

--- init.sh
+++ init.sh
@@ -1,2 +1,2 @@
-sudo ln -sf /home/Projects/nginx.conf  /etc/nginx/sites-enabled/default
+sudo ln -sf /home/Projects/Nginx/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
