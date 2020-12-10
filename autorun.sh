63c5937829bfce76b4df502d73686bdf1d2f5726
timur.borisevich <timurborisevich@mail.ru>
2020-12-10 10:40:31 +0300

    git

--- autorun.sh
+++ autorun.sh
@@ -1,3 +1,3 @@
 sudo /etc/init.d/nginx start
-sudo gunicorn -w 2 -c /home/project/URV_DetectWorkType/guicorn.py guicorn:app
+sudo gunicorn -w 2 -c /home/Projects/URV_DetectWorkType/guicorn.py guicorn:app
 
