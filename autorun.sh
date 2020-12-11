59efe6bb5a5a3f1aaf466325f02ffc6c4f9ffade
timur.borisevich <timurborisevich@mail.ru>
2020-12-11 17:30:35 +0300

    git

--- autorun.sh
+++ autorun.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-bash -c "cd /home/Projects/SpellChecker ; gunicorn -w 2 -c guicorn.py guicorn:app"
-bash -c "cd /home/Projects/URV_DetectWorkType ; gunicorn -w 2 -c guicorn.py guicorn:app"
+bash -c "screen ; cd /home/Projects/SpellChecker ; gunicorn -w 2 -c guicorn.py guicorn:app"
+bash -c "screen ; cd /home/Projects/URV_DetectWorkType ; gunicorn -w 2 -c guicorn.py guicorn:app"
